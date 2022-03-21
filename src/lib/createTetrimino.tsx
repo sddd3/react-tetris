5433267fe02b35ae094eff0f43cecac8b7cfb48a
sddd3 <gohoshiro@gmail.com>
2022-03-21 21:29:26 +0900

    createTetrimino

--- src/lib/createTetrimino.tsx
+++ src/lib/createTetrimino.tsx
@@ -1,4 +1,20 @@
-const createTetrimino = () => {
+const createTetrimino = (mino: string) => {
+    switch (mino) {
+        case 'I-Tetrimino':
+            return;
+        case 'J-Tetrimino':
+            return;
+        case 'L-Tetrimino':
+            return;
+        case 'O-Tetrimino':
+            return;
+        case 'S-Tetrimino':
+            return;
+        case 'T-Tetrimino':
+            return;
+        case 'Z-Tetrimino':
+            return;
+    }
 }
 
 export default createTetrimino
